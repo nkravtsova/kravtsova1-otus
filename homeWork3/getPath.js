2a5c587ad183fd67e5b11a7e1340bc3e6ac5cfa4
nkravtsova <nkravtsova@solit-clouds.ru>
2022-05-13 19:07:08 +0400

    v5

--- homeWork3/getPath.js
+++ homeWork3/getPath.js
@@ -1,30 +1,14 @@
 function getPath(currentEl){
 	let path = '';
 
-	while (true) {
-	  if(currentEl.hasOwnProperty('id')){
-
-		path = currentEl.tagName.toLowerCase() + '#' + currentEl.id + ' ' + path;
-
-	  } else if(document.getElementsByClassName(currentEl.className).length == 1){
-
-		path = currentEl.tagName.toLowerCase() + '.' + currentEl.className.split(' ').join('.') + ' ' + path;
-
-	  } else if(document.getElementsByTagName(currentEl.tagName).length == 1){
-
-		path = currentEl.tagName.toLowerCase() + ' '  + path;
-
-	  } else {
-
-		path = currentEl.tagName.toLowerCase() + ':nth-child(' + (+Array.from(currentEl.parentNode.children).indexOf(currentEl) + 1) + ') ' + path;
-
-	  }
+	while (currentEl) {
+		path = currentEl.tagName.toLowerCase() + ':nth-child(' + (+Array.from(currentEl.parentNode.children).indexOf(currentEl) + 1) + ')>' + path;
 
 	  currentEl = currentEl.parentNode; 
 	  if (currentEl.tagName == 'HTML') break;
 	}
 	
-	return path.trim();
+	return path.substr(0,path.length-1);
 }  
 
 module.exports = getPath;
