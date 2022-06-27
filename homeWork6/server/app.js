91b249a74cafa194b64519e688a392dfa4698707
Nataly <nkravtsova@solit-clouds.ru>
2022-06-27 20:38:25 +0400

    add source json edit app.js

--- homeWork6/server/app.js
+++ homeWork6/server/app.js
@@ -7,15 +7,17 @@ const app = express();
 const port = 3000;
 
 mongoose.connect('mongodb://localhost:27017/eCommerceShop', {useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use('/graphql', graphqlHTTP({
 	schema,
 	graphiql: true
 }));
 
-const dbConnection = mongoose.connection;
-dbConnection.on('error', err => console.log(err));
-dbConnection.once('open', () => console.log('connect'));
-
-app.listen(port, err => {
-	err? console.log(er): console.log('Graphql server started at localhost '+ port);
-});
+mongoose.connection.on('open', err => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Graphql server started at localhost '+ port);
+		app.listen(port);
+	}
+})
