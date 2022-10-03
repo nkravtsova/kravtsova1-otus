f7cb8b3e8778e06bbb8d7e426d2ebbca512d6238
Nataly <nkravtsova@solit-clouds.ru>
2022-10-03 13:04:22 +0400

    delete .nyc_output and format package

--- homeWork8/src/maxItemAssociation.ts
+++ homeWork8/src/maxItemAssociation.ts
@@ -1,21 +1,24 @@
 function maxItemAssociation(arr?: string[][] ){
 
 	if (arr){
-
 		const groups = new Set<Set<string>>();
 		for (const itemArray of arr){
-
 			if (groups.size == 0) {
+
 				groups.add(new Set<string>(itemArray));
 
 			} else {
 
 				for (const itemSet of groups){
+
 					let addArray = false; 
 					for (const itemGroups of itemSet){
+
 						if (itemArray.includes(itemGroups)){
-						itemArray.forEach(a => itemSet.add(a));
-						addArray = true;
+
+							itemArray.forEach(a => itemSet.add(a));
+							addArray = true;
+
 						}
 					}
 					if (!addArray) groups.add(new Set(itemArray));
@@ -26,8 +29,11 @@ function maxItemAssociation(arr?: string[][] ){
 	const result:string[][] = [];
 
 	for (const itemSet of groups) result.push(Array.from(itemSet).sort());
+
 		result.sort(function(a:string[], b:string[]):number {
+
 			return b.length - a.length || a[0].localeCompare(b[0]);
+
 		});
 
 	return result[0];
