44e36db8346141047dc7612d3434fb3b983b27b3
Nataly <nkravtsova@solit-clouds.ru>
2022-10-12 14:30:01 +0400

    delete build

--- homeWork8/register.js
+++ homeWork8/register.js
@@ -2,7 +2,7 @@ import tsNode from 'ts-node';
 import testTSConfig from './test/tsconfig.json' assert {type:"json"};
 
 tsNode.register({
-  files: true,
-  transpileOnly: true,
-  project: './test/tsconfig.json'
+	files: true,
+	transpileOnly: true,
+	project: './test/tsconfig.json'
 });
