52a21733c508b3a6ad7eb4091a1a31f0ca0c500b
nkravtsova <102795150+nkravtsova@users.noreply.github.com>
2022-10-12 17:52:41 +0400

    Revert "add homeWork8"

--- homeWork8/register.js
+++
@@ -1,8 +0,0 @@
-import tsNode from 'ts-node';
-import testTSConfig from './test/tsconfig.json' assert {type:"json"};
-
-tsNode.register({
-	files: true,
-	transpileOnly: true,
-	project: './test/tsconfig.json'
-});
